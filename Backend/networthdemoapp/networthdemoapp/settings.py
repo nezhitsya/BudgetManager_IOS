bc8313c473d7d4388347e651708a07cfe9a4063f
nezhitsya <huyn9295@naver.com>
2021-05-24 13:53:38 +0900

    set up db

--- Backend/networthdemoapp/networthdemoapp/settings.py
+++ Backend/networthdemoapp/networthdemoapp/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fqka!9%+69=a)jdnexc(aj2b7e@hh&u$^m3pvcn^ra4cs!j$c#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
