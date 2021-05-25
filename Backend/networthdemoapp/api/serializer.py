cf57c867441480f58bcd2ce56350a44b740023bf
nezhitsya <huyn9295@naver.com>
2021-05-25 11:57:29 +0900

    set up db

--- Backend/networthdemoapp/api/serializer.py
+++ Backend/networthdemoapp/api/serializer.py
@@ -1,1 +1,6 @@
 from .models import Account
+
+class AccountSerializer(serializers.ModelSerializer) :
+    class Meta :
+        model = Account
+        fields = ['name', 'category', 'description', 'wealth_type', 'balance']
