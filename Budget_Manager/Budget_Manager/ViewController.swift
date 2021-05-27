a00e337598ee0b9ea5672f2236b28dfb2653f101
nezhitsya <huyn9295@naver.com>
2021-05-27 14:19:02 +0900

    first commit

---
+++ Budget_Manager/Budget_Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget_Manager
+//
+//  Created by 이다영 on 2021/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
