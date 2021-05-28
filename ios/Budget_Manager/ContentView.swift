b4379cb705594109daaabc14135cc9397ea9434d
nezhitsya <huyn9295@naver.com>
2021-05-28 10:47:22 +0900

    set up swift ui

--- ios/Budget_Manager/ContentView.swift
+++ ios/Budget_Manager/ContentView.swift
@@ -9,72 +9,120 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State var accounts = [Account]()
+    @State var showAdd = false
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        NavigationView {
+            List {
+                ForEach(accounts) { item in
+                    HStack {
+                        Image(systemName: "banknote").foregroundColor(.green)
+                        Text(item.name)
+                        Spacer()
+                        Text("\(item.balance)")
+                    }
+                }
+            }.onAppear(perform: loadAccount)
+            .navigationBarTitle("Accounts")
+            .navigationBarItems(trailing: Button(action: {showAdd.toggle()}, label: {
+                Image(systemName: "plus.circle")
+            }))
+            .listStyle(PlainListStyle())
+            .sheet(isPresented: $showAdd, content: {
+                AccountAddview(function: self.loadAccount)
+            })
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    
+    func loadAccount() {
+        guard let url = URL(string: "http://127.0.0.1:8000/api/account") else {
+        return
         }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let response = try? JSONDecoder().decode([Account].self, from: data) {
+                    self.accounts = response
+                }
+            }
+            
+        }.resume()
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct AccountAddview: View {
+    @Environment(\.presentationMode) var presentationMode
+    var function: () -> Void
+    @State var name: String = ""
+    @State var category: String = ""
+    @State var description: String = ""
+    @State var wealth_type: String = ""
+    @State var balance: String = ""
+    
+    var categories = ["Asset", "Liabilities"]
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Account Name", text: $name)
+                    Picker("Category", selection: $category) {
+                        ForEach(categories, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    TextField("Balance", text: $balance)
+                    TextField("Wealth Type", text: $wealth_type)
+                    TextField("Description", text: $description)
+                }
+            }.listStyle(GroupedListStyle())
+            .navigationBarTitle("Add Account")
+            .navigationBarItems(leading: Button("Cancel") {
+                presentationMode.wrappedValue.dismiss()
+            }, trailing: Button(action: { postAccount() }, label: {
+                Text("Save")
+            }))
+        }
+    }
+    
+    func postAccount() {
+        guard let url = URL(string: "http://127.0.0.1:8000/api/account/") else {
+            return
+        }
+        
+        let accountData = Account(id: 0, name: self.name, category: self.category, description: self.description, wealth_type: self.wealth_type, balance: Int(self.balance) ?? 0, created_at: "")
+        
+        guard let encoded = try? JSONEncoder().encode(accountData) else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic ", forHTTPHeaderField: "Authorization")
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let response = try? JSONDecoder().decode(Account.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.function()
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    return
+                }
+            }
+        }.resume()
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
