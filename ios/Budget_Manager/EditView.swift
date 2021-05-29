dc68d6f2832919e80af740b24e29a8dca9499a2c
nezhitsya <huyn9295@naver.com>
2021-05-29 15:58:53 +0900

    set up swift ui

--- ios/Budget_Manager/EditView.swift
+++ ios/Budget_Manager/EditView.swift
@@ -12,8 +12,65 @@ struct EditView: View {
     @Environment(\.presentationMode) var presentationMode
     @Binding var account: Account
     
+    var categories = ["Asset", "Liabilities"]
+    var wealth_types = ["Wealth Building", "Non-Wealth Building"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Account Name", text: $account.name)
+                    Picker("Category", selection: $account.category) {
+                        ForEach(categories, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    TextField("Balance", value: $account.balance, formatter: NumberFormatter())
+                }
+                TextField("Description", text: $account.description)
+                Picker("Wealth Type", selection: $account.wealth_type) {
+                    ForEach(wealth_types, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }.listStyle(GroupedListStyle())
+            .navigationTitle(Text("Edit Account"))
+            .navigationBarItems(leading: Button("Cancel") {
+                presentationMode.wrappedValue.dismiss()
+            }, trailing: Button(action: { updateAccount() }, label: {
+                Text("Save")
+            }))
+        }
+    }
+    
+    func updateAccount() {
+        guard let url = URL(string: "http://127.0.0.1:8000/api/account/\(self.account.id)/") else {
+            fatalError()
+        }
+        
+        let accountData = self.account
+        
+        guard let encoded = try? JSONEncoder().encode(accountData) else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.addValue("application/JSON", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic ", forHTTPHeaderField: "Authorization")
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let response = try? JSONDecoder().decode(Account.self, from: data) {
+                    DispatchQueue.main.async {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    return
+                }
+            }
+        }.resume()
     }
 }
 
